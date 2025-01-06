0c0f70160d61f0403220f21988cc05b9b4a1fd5e
aswathik662 <aswathikudayan@gmail.com>
2025-01-06 09:29:36 +0530

    first commit

---
+++ swit.js
@@ -0,0 +1,25 @@
+let a=2;
+switch(a)
+{
+    case 0:
+        console.log('sunday')
+        break;
+        case 1:
+            console.log('monday')
+            break;
+            case 2:
+                console.log('tuesday')
+                break;
+                case 3:
+                    console.log('wednesday')
+                    break;
+                    case 4:
+                        console.log('thursday')
+                        break;
+                        case 5:
+                            console.log('friday')
+                            break;
+                            default:
+                                console.log('saturday')
+
+}
